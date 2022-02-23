9a66865a96280a55947f9d23821709a8903638b9
vky <gillv625@gmail.com>
2022-02-23 23:01:21 +0100

    hotfix key registry

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Keyboard from './components/Keyboard';
 
 function App() {
   const [grid, setGrid] = useState(Array.from({ length: 6 }, () => Array.from({ length: 5 }, () => '')));
-  const [current, setCurrent] = useState({ x: 0, y: 0 })
+  const [current, setCurrent] = useState({ row: 0, _col: 0 })
 
   useEffect(() => {
     (async () => {
@@ -16,13 +16,64 @@ function App() {
   }, []);
 
   const handLetter = (letter) => {
-    for (let y = 0; y < 6; y++) {
-      for (let x = 0; x < 5; x++) {
-        let aux = grid[y][x];
-        grid[y][x] = !grid[y][x] ? letter : grid[y][x];
-        if(!aux) break
+    let row = current.row
+    let hayVacio = false
+    let col = 0
+    while (col < grid[row].length && !hayVacio) {
+      if (grid[row][col] == "") {
+        // setCurrent({ x: row, y: col })
+        hayVacio = true
       }
+      col++
     }
+
+
+    switch (letter) {
+      case "ENTER":
+        if (hayVacio) {
+          alert("Completa la palabra")
+        } else {
+          console.log("ok");
+          if (row <= 4) {
+            setCurrent({ row: current.row + 1 })
+          }
+        }
+
+
+        break
+      case "DELETE": console.log("enter");
+        break
+      default:
+
+
+        if (hayVacio) {
+          let gridCopy = [...grid];
+          gridCopy[row][col - 1] = letter
+          setGrid(gridCopy)
+        }
+
+    }
+
+    // check row
+    // while (row < grid.length && !encontrado) {
+    //   if (grid[row][0] == "") {
+    //     setCurrent({ row: row, col: current.y })
+    //     encontrado = true
+    //   } else {
+    //     row++
+    //   }
+
+    // }
+
+
+    // console.log(letter);
+    // for (let y = 0; y < 6; y++) {
+    //   for (let x = 0; x < 5; x++) {
+    //     let aux = grid[y][x];
+    //     grid[y][x] = !grid[y][x] ? letter : grid[y][x];
+    //     if (!aux) break
+    //   }
+    // }
   }
 
   return (
